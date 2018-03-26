24c20316741622d1d9238e4f98b92b4407232eb7
Aleksandr Roslyakov <torpid@gmail.com>
2018-03-26 19:39:50 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -28,6 +28,7 @@ def until_iterator(number_of_times)
   until number_of_times == 0
     phrase = "Welcome to Flatiron School's Web Development Course!"
     puts phrase
+    number_of_times -= 1
   end
 end
 
